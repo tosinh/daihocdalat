a8a5209da76502e0538f3e84d6bcd946d22a7edf
tosinh <tosinh680@gmail.com>
2023-05-08 20:10:05 +0700

    add carousel

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 <script>
-    
+    import Carousel from "../components/carousel.svelte";
+
+    const images = [
+        {
+            id: "anh1",
+            path: "../src/public/anh1.jpg",
+        },
+        {
+            id: "anh2",
+            path: "../src/public/anh2.png",
+        },
+        {
+            id: "anh3",
+            path: "../src/public/anh3.png",
+        },
+    ];
 </script>
 
 <svelte:head>
     <title>Trường đại học Đà Lạt</title>
 </svelte:head>
 
+<section class="flex flex-col items-center">
+    <Carousel {images} imageWhidth={1200} imageSpacing={15} />
+
+    <slot />
+</section>
